72e9e21dddda5a2a6d1ad54fe1816b8c283a1170
Kowhai-tree <70258696+Kowhai-tree@users.noreply.github.com>
2020-08-28 12:33:17 +1200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
       ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
   
-  speech += speech + "Repeated"
+  speech = speech + "Repeated"
   
   var speechResponse = {
     google: {
